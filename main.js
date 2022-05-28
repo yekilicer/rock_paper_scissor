8d8cd484ebe4ac396faaca807d139a58bc6c7024
yekilicer <kilicer.ye@gmail.com>
2022-05-29 01:29:02 +0300

    script file modified to play compatible with ui

--- main.js
+++ main.js
@@ -1,12 +1,53 @@
+// global variables
 const choices = ["rock", "paper", "scissors"];
+let player_wins = 0;
+let computer_wins = 0;
+let rounds_played = 0;
 
-function computerPlay()
-{
+let player_selection = "";
+let computer_selection = "";
 
+const selection_highlight_duration = 1000;
+
+
+function play(e) {
+    // multiple selections in a short period of time causes highlight bug.
+    if (player_selection !== "") return;
+
+    // get element references
+    player_selection = e.target.parentNode;
+
+    computer_selection = document.querySelector(`.computer-container .${choices[Math.floor(Math.random() * choices.length)]}`).parentNode;
+
+    
+    isPlayerScored(player_selection, computer_selection) ? player_wins++ : computer_wins++;
+    console.log(isPlayerScored(player_selection, computer_selection));
+    console.log(player_selection);
+    console.log(computer_selection);
+    console.log(player_wins);
+    console.log(computer_wins);
+    updateScores();
+    highlightSelections();
+    setTimeout(resetSelections, selection_highlight_duration);
 }
 
-function isVictory(player_selection, computer_selection)
+function highlightSelections(){
+    player_selection.classList.add('player-selected');
+    computer_selection.classList.add('computer-selected');
+    setTimeout(() => player_selection.classList.remove('player-selected'), selection_highlight_duration);
+    setTimeout(() => computer_selection.classList.remove('computer-selected'), selection_highlight_duration);
+}
+
+function resetSelections() {
+    player_selection = "";
+    computer_selection = "";
+}
+
+function isPlayerScored(player_selection, computer_selection)
 {
+    player_selection = player_selection.lastElementChild.className;
+    computer_selection = computer_selection.lastElementChild.className;
+
     if (player_selection === "rock")
     {
         if (computer_selection === "scissors") return true;
@@ -27,34 +68,13 @@ function isVictory(player_selection, computer_selection)
 
 }
 
-function game()
-{
-    let number_of_games = prompt("HOW MANY ROUNDS YOU WANT TO PLAY?", 5);
-    let player_wins = 0;
-    let computer_wins = 0;
-    let rounds_played = 0;
-
-
-    while(rounds_played<number_of_games)
-    {
-        let computer_selection = choices[Math.floor(Math.random() * choices.length)];
-        let player_selection = prompt("ROCK PAPER SCISSOR?").toLowerCase();
-        if (!choices.includes(player_selection))
-        {
-            alert("you made and unavailable selection!");
-            continue;
-        }
-        
-        if (isVictory(player_selection, computer_selection)) player_wins++;
-        else computer_wins++;
-        rounds_played++;
-
-        console.log(`player : ${player_selection}`);
-        console.log(`computer : ${computer_selection}`);
-        console.log(`player wins: ${player_wins}`);
-        console.log(`computer_wins: ${computer_wins}`);
-        console.log(`rounds_played: ${rounds_played}`);
-    }
+function updateScores() {
+    const player_score = document.querySelector(".player-score");
+    player_score.textContent = `SCORE: ${player_wins}`;
+    const computer_score = document.querySelector(".computer-score");
+    computer_score.textContent =`SCORE: ${computer_wins}`;
 }
 
-game();
+const player_selections = document.querySelectorAll('.player-container .selection');
+
+player_selections.forEach(selection => { selection.addEventListener('click', play)});
